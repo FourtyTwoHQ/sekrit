0ec50498720499bc52f4152cff5850fc8ed25c1c
Ben Sarrazin <b@srz.io>
2019-07-20 21:13:28 -0400

    Writing, reading, encrypting, decrypting

--- lib/sekrit/pull.rb
+++ lib/sekrit/pull.rb
@@ -5,7 +5,9 @@ module Sekrit
         def initialize(bundle_id: String, config: Config, passphrase: String)
             @bundle_id = bundle_id
             @config = config
-            @encoder = Decoder.new(password: passphrase)
+            @decoder = Decoder.new(password: passphrase)
+
+            raise Thor::Error, Rainbow("Bundle id cannot be nil").red if @bundle_id.nil?
         end
 
         def copy_bundled_files(dir: String)
@@ -14,22 +16,62 @@ module Sekrit
 
             bundled_files = @config.bundled_files.files + bundle.files
             bundled_files.each do |f|
-                puts "Copying '#{dir}/#{bundle.id}/#{f}' to '#{f}'"
+                src = "#{dir}/#{bundle.id}/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(src)
+                    log.debug "Preparing to copy '#{src}' to '#{file_path}'"
+                    FileUtils.cp(src, f)
+                    log.debug "Copied '#{src}' to '#{file_path}'"
+                else
+                    log.warn("Could not find file at path '#{src}'")
+                end
             end
 
             encrypted_files = @config.bundled_files.encrypted + bundle.encrypted
             encrypted_files.each do |f|
-                puts "Decryptingg and copying '#{dir}/#{bundle.id}/#{f}' to '#{f}'"
+                src = "#{dir}/#{bundle.id}/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(src)
+                    log.debug "Preparing to decrypt and copy '#{src}' to '#{file_path}'"
+                    FileUtils.cp(src, f)
+                    log.debug "Copied '#{src}' to '#{file_path}'"
+
+                    log.debug "Preparing to decrypt '#{file_path}'"
+                    File.write(file_path, @decoder.decode(string: File.read(file_path)))
+                    log.debug "Decrypted '#{file_path}'"
+                else
+                    log.warn("Could not find file at path '#{src}'")
+                end
             end
         end
 
         def copy_shared_files(dir: String)
             @config.shared_files.files.each do |f|
-                puts "Copying '#{dir}/shared/#{f}' to '#{f}'"
+                src = "#{dir}/shared/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(src)
+                    log.debug "Preparing to copy '#{src}' to '#{file_path}'"
+                    FileUtils.cp(src, f)
+                    log.debug "Copied '#{src}' to '#{file_path}'"
+                else
+                    log.warn("Could not find file at path '#{src}'")
+                end
             end
 
             @config.shared_files.encrypted.each do |f|
-                puts "Decryptingg and copying '#{dir}/shared/#{f}' to '#{f}'"
+                src = "#{dir}/shared/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(src)
+                    log.debug "Preparing to decrypt and copy '#{src}' to '#{file_path}'"
+                    FileUtils.cp(src, f)
+                    log.debug "Copied '#{src}' to '#{file_path}'"
+
+                    log.debug "Preparing to decrypt '#{file_path}'"
+                    File.write(file_path, @decoder.decode(string: File.read(file_path)))
+                    log.debug "Dencrypted '#{file_path}'"
+                else
+                    log.warn("Could not find file at path '#{src}'")
+                end
             end
         end
     end
--- lib/sekrit/push.rb
+++ lib/sekrit/push.rb
@@ -6,6 +6,8 @@ module Sekrit
             @bundle_id = bundle_id
             @config = config
             @encoder = Encoder.new(password: passphrase)
+
+            raise Thor::Error, Rainbow("Bundle id cannot be nil").red if @bundle_id.nil?
         end
 
         def copy_bundled_files(dir: String)
@@ -14,22 +16,66 @@ module Sekrit
 
             bundled_files = @config.bundled_files.files + bundle.files
             bundled_files.each do |f|
-                puts "Copying '#{f}' to '#{dir}/#{bundle.id}/#{f}'"
+                dest = "#{dir}/#{bundle.id}/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(file_path)
+                    log.debug "Preparing to copy '#{file_path}' to '#{dest}'"
+                    FileUtils.mkdir_p(File.dirname(dest))
+                    FileUtils.cp(f, dest)
+                    log.debug "Copied '#{file_path}' to '#{dest}'"
+                else
+                    log.warn("Could not find file at path '#{file_path}'")
+                end
             end
 
             encrypted_files = @config.bundled_files.encrypted + bundle.encrypted
             encrypted_files.each do |f|
-                puts "Encrypting and copying '#{f}' to '#{dir}/#{bundle.id}/#{f}'"
+                dest = "#{dir}/#{bundle.id}/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(file_path)
+                    log.debug "Preparing to encrypt and copy '#{file_path}' to '#{dest}'"
+                    FileUtils.mkdir_p(File.dirname(dest))
+                    FileUtils.cp(f, dest)
+                    log.debug "Copied '#{file_path}' to '#{dest}'"
+
+                    log.debug "Preparing to encrypt '#{dest}'"
+                    File.write(dest, @encoder.encode(string: File.read(dest)))
+                    log.debug "Encrypted '#{dest}'"
+                else
+                    log.warn("Could not find file at path '#{file_path}'")
+                end
             end
         end
 
         def copy_shared_files(dir: String)
             @config.shared_files.files.each do |f|
-                puts "Copying '#{f}' to '#{dir}/shared/#{f}'"
+                dest = "#{dir}/shared/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(file_path)
+                    log.debug "Preparing to copy '#{file_path}' to '#{dest}'"
+                    FileUtils.mkdir_p(File.dirname(dest))
+                    FileUtils.cp(f, dest)
+                    log.debug "Copied '#{file_path}' to '#{dest}'"
+                else
+                    log.warn("Could not find file at path '#{file_path}'")
+                end
             end
 
             @config.shared_files.encrypted.each do |f|
-                puts "Encrypting and copying '#{f}' to '#{dir}/shared/#{f}'"
+                dest = "#{dir}/shared/#{f}"
+                file_path = "#{Dir.pwd}/#{f}"
+                if File.exist?(file_path)
+                    log.debug "Preparing to encrypt and copy '#{file_path}' to '#{dest}'"
+                    FileUtils.mkdir_p(File.dirname(dest))
+                    FileUtils.cp(f, dest)
+                    log.debug "Copied '#{file_path}' to '#{dest}'"
+
+                    log.debug "Preparing to encrypt '#{dest}'"
+                    File.write(dest, @encoder.encode(string: File.read(dest)))
+                    log.debug "Encrypted '#{dest}'"
+                else
+                    log.warn("Could not find file at path '#{file_path}'")
+                end
             end
         end
     end
